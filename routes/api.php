b1b5144e16befa85e42818dd649a14ce841482af
amine mazza <aminemazza6@gmail.com>
2023-12-27 03:04:22 +0100

    sécurisé les routes(Web-api)

--- routes/api.php
+++ routes/api.php
@@ -27,25 +27,32 @@ Route::group(['middleware'=>['auth:sanctum']] ,function() {
 Route::post("/register",[AuthApicontroller::class,"register"]);
 Route::post("/login",[AuthApicontroller::class,"login"]);
 
-// Pour recuperer la liste des produits :
-Route::get("/products",[ProductController::class,"getProduct"])->name("products");
 
-// Creer un produit :
-Route::post('/products/create', [ProductController::class,'create']);
+Route::middleware(['admin'])->group(function () {
+    // Pour recuperer la liste des produits :
+    Route::get("/products",[ProductController::class,"getProduct"])->name("products");
+    // Creer un produit :
+    Route::post('/products/create', [ProductController::class,'create']);
 
-// Modifier un produit :
-Route::put('/products/edit/{id}', [ProductController::class,'update']);
+    // Modifier un produit :
+    Route::put('/products/edit/{id}', [ProductController::class,'update']);
+});
 
 
- //liste des categories
- Route::get('/categories', [CategoryController::class, 'getCategories'])->name("categories");
+Route::middleware(['admin'])->group(function () {
+    //liste des categories
+    Route::get('/categories', [CategoryController::class, 'getCategories'])->name("categories");
 
- // Endpoint pour créer une nouvelle catégorie
- Route::post('/categories/create', [CategoryController::class, 'create']);
- 
- // Endpoint pour mettre à jour une catégorie existante
- Route::put('/categories/update/{id}', [CategoryController::class, 'update']);
-  
+    // Endpoint pour créer une nouvelle catégorie
+    Route::post('/categories/create', [CategoryController::class, 'create']);
+    
+    // Endpoint pour mettre à jour une catégorie existante
+    Route::put('/categories/update/{id}', [CategoryController::class, 'update']);
+});  
+
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
    
