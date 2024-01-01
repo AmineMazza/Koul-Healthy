efac3298985e238479dad5bf524f02035e09ceac
amine mazza <aminemazza6@gmail.com>
2024-01-01 23:50:39 +0100

    modifier routeAPI

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\AuthApicontroller;
+use App\Http\Controllers\Api\AuthApicontroller;
 use App\Http\Controllers\UserController;
 
 
@@ -19,39 +19,39 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::group(['middleware'=>['auth:sanctum']] ,function() {
-    Route::post("/logout", [AuthApiController::class,"logout"]);
-});
 
 //route vers auth API(login et register)
-Route::post("/register",[AuthApicontroller::class,"register"]);
-Route::post("/login",[AuthApicontroller::class,"login"]);
+Route::post('/register',[AuthApicontroller::class,"register"]);
+Route::post('/login',[AuthApicontroller::class,"login"]);
+Route::post('/logout', [AuthApiController::class,"logout"]);
 
 
-    // Pour recuperer la liste des produits :
-    Route::get("/products",[ProductController::class,"getProduct"])->name("products");
-    // Creer un produit :
-    Route::post('/products/create', [ProductController::class,'create']);
+Route::group(['middleware'=>['auth:sanctum']] ,function() {
 
-    // Modifier un produit :
-    Route::put('/products/edit/{id}', [ProductController::class,'update']);
+     // Pour recuperer la liste des produits :
+     Route::get("/products",[ProductController::class,"getProduct"])->name("products");
+     // Creer un produit :
+     Route::post('/products/create', [ProductController::class,'create']);
+     // Modifier un produit :
+     Route::put('/products/edit/{id}', [ProductController::class,'update']);
+ 
+ 
+     //liste des categories
+     Route::get('/categories', [CategoryController::class, 'getCategories'])->name("categories");
+     // Endpoint pour créer une nouvelle catégorie
+     Route::post('/categories/create', [CategoryController::class, 'create']);
+     // Endpoint pour mettre à jour une catégorie existante
+     Route::put('/categories/update/{id}', [CategoryController::class, 'update']);
 
-
-
-    //liste des categories
-    Route::get('/categories', [CategoryController::class, 'getCategories'])->name("categories");
-
-    // Endpoint pour créer une nouvelle catégorie
-    Route::post('/categories/create', [CategoryController::class, 'create']);
-    
-    // Endpoint pour mettre à jour une catégorie existante
-    Route::put('/categories/update/{id}', [CategoryController::class, 'update']);
-
-
-
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-   
+     Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+     
 });
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+   
+// });
